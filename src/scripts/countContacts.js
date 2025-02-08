a30b911c14db61bf41f2830fa0f7317de8cbd850
Pavlo-Chernicenko <pwned7067@gmail.com>
2025-02-09 00:23:48 +0200

    Completed all the tasks

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,11 @@
-export const countContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const countContacts = async () => {
+  try {
+    return (await readContacts()).length;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 console.log(await countContacts());
